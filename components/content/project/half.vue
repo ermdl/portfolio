dcd017d6f9cc1e972a461dc016b728c8f5f01432
Danila Ermolenko <danila@fluco.studio>
2024-07-03 17:05:12 +0100

    Some changes to layout

--- components/content/project/half.vue
+++ components/content/project/half.vue
@@ -3,9 +3,7 @@
     <div class="col-span-1 flex flex-col items-start justify-end">
       <ContentSlot :use="$slots.text"></ContentSlot>
     </div>
-    <div
-      class="col-span-1 flex flex-col items-start justify-end rounded-3xl overflow-hidden"
-    >
+    <div class="col-span-1 flex flex-col items-start justify-end">
       <ContentSlot :use="$slots.image"></ContentSlot>
     </div>
   </section>
